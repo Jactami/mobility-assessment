3ad70b98717c41f648d725bb91a4e007acd16658
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-05-02 19:20:01 +0200

    base header + section

---
+++ src/components/base/BasePageHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="text-4xl font-medium" :class="{ 'mb-10': !subtitle }">{{ title }}</h1>
+  <p v-if="subtitle" class="mt-6 mb-10 text-lg text-on-surface-variant">{{ subtitle }}</p>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string
+  subtitle?: string
+}>()
+</script>
---
+++ src/components/base/BaseSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <h2 v-if="title" class="mt-6 text-xl font-bold">{{ title }}</h2>
+    <div class="pt-4 pb-8">
+      <slot>
+        <!-- Section content goes here. -->
+      </slot>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title?: string
+}>()
+</script>
