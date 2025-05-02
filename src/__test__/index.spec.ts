287c4f2b843e6ce7c4e223f3e69cbbc332fd3507
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-05-02 16:11:09 +0200

    clean up

--- src/__test__/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest'
-
-describe('test vitest integration', () => {
-  it('return correct validation', async () => {
-    expect(1 + 2).toBe(3)
-    expect(['a', 'b', 'c']).toHaveLength(3)
-  })
-})
--- src/components/TestComponent.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <p class="text-xl font-bold">I am a test component.</p>
-</template>
