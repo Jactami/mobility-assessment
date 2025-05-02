7dddf57c70cb42a5a08f561615d130794c8c417b
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-05-02 19:19:25 +0200

    app base layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <main>
-    <RouterView />
-  </main>
+  <div class="grid min-h-screen">
+    <div class="flex flex-col">
+      <main class="container mx-auto flex grow flex-col px-4 pt-6 pb-24 lg:px-10 lg:pb-10">
+        <RouterView />
+      </main>
+    </div>
+  </div>
 </template>
