85d46871845aba22cc67c62e8592441c0c90049f
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-05-04 13:50:03 +0200

    fetch data from supabase

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,18 @@
 <template>
   <h1>Home</h1>
+
+  <DebugPanel title="Supabae Project" :value="projects" />
 </template>
+
+<script setup lang="ts">
+import DebugPanel from '@/components/debug/DebugPanel.vue'
+import { supabase } from '@/db'
+import { onMounted, ref } from 'vue'
+
+const projects = ref()
+
+onMounted(async () => {
+  const { data } = await supabase.from('projects').select()
+  projects.value = data
+})
+</script>
