aa81d5b7d61ec1ccbb5106d52006afaa28f25086
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-05-04 21:19:51 +0200

    update project seed

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,5 +1,8 @@
 --
--- Create project
+-- Create projects
 --
 INSERT INTO projects (id, title, zip_code, city, street, street_number) 
 VALUES ('00000000-0000-0000-0000-000000000000', 'ERBA', '96049', 'Bamberg', 'An der Weberei', '5');   
+
+INSERT INTO projects (id, title, zip_code, city, street, street_number) 
+VALUES ('11111111-1111-1111-1111-111111111111', 'Feki', '96052', 'Bamberg', 'Feldkirchenstraße', '21');   
