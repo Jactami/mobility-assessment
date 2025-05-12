506fa1d28611cdd89f3f1773ecfd4727883f9884
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-05-12 14:22:04 +0200

    remove footer

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
     <main class="container mx-auto flex grow flex-col px-4 pt-6 pb-24 lg:px-10 lg:pb-10">
       <RouterView />
     </main>
-    <footer class="bg-red-200">Test</footer>
   </div>
 </template>
 
