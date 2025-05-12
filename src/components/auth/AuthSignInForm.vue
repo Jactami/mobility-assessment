fbb6377f1244d4b40071739e3a499afd065548cf
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-05-12 14:42:30 +0200

    password visibility toggler

--- src/components/auth/AuthSignInForm.vue
+++ src/components/auth/AuthSignInForm.vue
@@ -13,13 +13,28 @@
       />
       <FormKit
         id="password"
-        type="password"
+        :type="passwordVisible ? 'text' : 'password'"
         :label="t('auth.password')"
         name="password"
         :placeholder="t('auth.password')"
         validation="required"
-      />
-      <BaseButton type="submit" class="w-full" :disabled="!valid">Login</BaseButton>
+      >
+        <template #suffix>
+          <button
+            type="button"
+            tabindex="-1"
+            class="cursor-pointer p-0.5 hover:text-primary"
+            @mousedown="passwordVisible = true"
+            @mouseup="passwordVisible = false"
+          >
+            <MaterialSymbolsVisibilityOffOutlineRounded v-if="passwordVisible" aria-hidden="true" />
+            <MaterialSymbolsVisibilityOutlineRounded v-else aria-hidden="true" />
+          </button>
+        </template>
+      </FormKit>
+      <BaseButton type="submit" classes="w-full" :disabled="!valid">
+        {{ t('auth.login') }}
+      </BaseButton>
     </FormKit>
   </div>
 </template>
@@ -27,9 +42,14 @@
 <script setup lang="ts">
 import BaseButton from '@/components/base/BaseButton.vue'
 import { useLogger } from '@/composables/log'
+import { ref } from 'vue'
 import { useI18n } from 'vue-i18n'
+import MaterialSymbolsVisibilityOffOutlineRounded from '~icons/material-symbols/visibility-off-outline-rounded'
+import MaterialSymbolsVisibilityOutlineRounded from '~icons/material-symbols/visibility-outline-rounded'
 
 const { t } = useI18n()
+
+const passwordVisible = ref(false)
 
 function handleSignIn(data: unknown) {
   useLogger().log('TODO: sign in', data)
