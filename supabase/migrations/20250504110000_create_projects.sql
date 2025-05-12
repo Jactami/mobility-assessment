a87211690a9a9ced2cb1d6f7876239ebb0829ffb
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-05-12 21:27:00 +0200

    create rls policies for projects

--- supabase/migrations/20250504110000_create_projects.sql
+++ supabase/migrations/20250504110000_create_projects.sql
@@ -23,8 +23,33 @@ ALTER TABLE ONLY public.projects
 ALTER TABLE ONLY public.projects
     ADD CONSTRAINT projects_owner_id_fkey FOREIGN KEY (owner_id) REFERENCES auth.users(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
+-- Moddatetime extension
 CREATE TRIGGER handle_updated_at_projects
     BEFORE UPDATE ON public.projects
     FOR EACH ROW
     EXECUTE FUNCTION moddatetime('updated_at');
 
+-- RLS
+ALTER TABLE public.projects ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Authenticated users can read their own projects." ON public.projects FOR SELECT
+TO authenticated
+USING ((SELECT auth.uid()) = owner_id);
+
+CREATE POLICY "Authenticated users can insert their own projects." ON public.projects FOR INSERT
+TO authenticated
+WITH CHECK ((SELECT auth.uid()) = owner_id);
+
+CREATE POLICY "Authenticated users can update their own projects." ON public.projects FOR UPDATE
+TO authenticated
+USING ((SELECT auth.uid()) = owner_id);
+
+CREATE POLICY "Authenticated users can delete their own projects." ON public.projects FOR DELETE
+TO authenticated
+USING ((SELECT auth.uid()) = owner_id);
+
+REVOKE ALL ON TABLE public.projects FROM anon;
+
+GRANT ALL ON TABLE public.projects TO authenticated;
+
+GRANT ALL ON TABLE public.projects TO service_role;
--- supabase/seed.sql
+++ supabase/seed.sql
@@ -3,6 +3,8 @@
 --
 SELECT public.create_user(email => 'max@mustermann.de', PASSWORD => 'password');
 
+SELECT public.create_user(email => 'john@doe.com', PASSWORD => 'password');
+
 --
 -- Create projects
 --
@@ -14,4 +16,9 @@ VALUES ('00000000-0000-0000-0000-000000000000', 'ERBA', '96049', 'Bamberg', 'An 
 INSERT INTO projects (id, title, zip_code, city, street, street_number, owner_id) 
 VALUES ('11111111-1111-1111-1111-111111111111', 'Feki', '96052', 'Bamberg', 'Feldkirchenstraße', '21', (
     SELECT id FROM auth.users WHERE email = 'max@mustermann.de'
+));   
+
+INSERT INTO projects (id, title, zip_code, city, street, street_number, owner_id) 
+VALUES ('33333333-3333-3333-3333-333333333333', 'Austraße', '96047 ', 'Bamberg', 'Austraße', '37', (
+    SELECT id FROM auth.users WHERE email = 'john@doe.com'
 ));   
