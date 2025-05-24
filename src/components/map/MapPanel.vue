9be20d30ff63b82f56328b6ed432cc508289754c
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-05-24 15:11:46 +0200

    development map

--- src/components/map/MapPanel.vue
+++ src/components/map/MapPanel.vue
@@ -3,27 +3,44 @@
 </template>
 
 <script setup lang="ts">
+import { useGeolocation } from '@vueuse/core'
+import { defaults as defaultControls, FullScreen, ScaleLine, Zoom } from 'ol/control'
 import TileLayer from 'ol/layer/Tile'
 import Map from 'ol/Map'
-import XYZ from 'ol/source/XYZ'
+import { fromLonLat } from 'ol/proj'
+import { OSM } from 'ol/source'
 import View from 'ol/View'
-import { onMounted, onUnmounted, ref } from 'vue'
+import { onMounted, onUnmounted, ref, watch } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
 
 const mapEl = ref<HTMLDivElement | null>(null)
 
+const { coords, pause } = useGeolocation({ immediate: true, enableHighAccuracy: true })
+
 const map = new Map({
-  target: 'map',
   layers: [
     new TileLayer({
-      source: new XYZ({
-        url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-      }),
+      source: new OSM(),
     }),
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 2,
+    center: fromLonLat([10.4477, 51.1634]), // Centered on Germany
+    zoom: 5.5,
   }),
+  controls: defaultControls().extend([
+    new FullScreen({
+      tipLabel: t('map.toggleFullscreen'),
+    }),
+    new Zoom({
+      zoomInTipLabel: t('map.zoomIn'),
+      zoomOutTipLabel: t('map.zoomOut'),
+    }),
+    new ScaleLine({
+      units: 'metric',
+    }),
+  ]),
 })
 
 onMounted(() => {
@@ -34,5 +51,14 @@ onMounted(() => {
 
 onUnmounted(() => {
   map.setTarget(undefined)
+})
+
+watch(coords, (newCoords) => {
+  const { latitude, longitude } = newCoords
+  if (latitude && longitude) {
+    map.getView().setCenter(fromLonLat([longitude, latitude]))
+    map.getView().setZoom(13)
+    pause() // Pause geolocation updates after centering the map
+  }
 })
 </script>
