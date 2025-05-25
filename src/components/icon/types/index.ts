41ba4cc287f8e8e1ba82ba1dd3fd251f606add65
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-05-25 17:13:11 +0200

    development geocode search

--- src/components/icon/types/index.ts
+++ src/components/icon/types/index.ts
@@ -1,9 +1,11 @@
 import MaterialSymbolsAddCircleOutline from '~icons/material-symbols/add-circle-outline'
 import MaterialSymbolsCheckRounded from '~icons/material-symbols/check-rounded'
+import MaterialSymbolsCloseRounded from '~icons/material-symbols/close-rounded'
 import MaterialSymbolsContentCopyOutlineRounded from '~icons/material-symbols/content-copy-outline-rounded'
 import MaterialSymbolsDeleteOutlineRounded from '~icons/material-symbols/delete-outline-rounded'
 import MaterialSymbolsLogoutRounded from '~icons/material-symbols/logout-rounded'
 import MaterialSymbolsMoreVert from '~icons/material-symbols/more-vert'
+import MaterialSymbolsSearchRounded from '~icons/material-symbols/search-rounded'
 import MaterialSymbolsVisibilityOffOutlineRounded from '~icons/material-symbols/visibility-off-outline-rounded'
 import MaterialSymbolsVisibilityOutlineRounded from '~icons/material-symbols/visibility-outline-rounded'
 import TablerExternalLink from '~icons/tabler/external-link'
@@ -18,6 +20,8 @@ export const iconMap = {
   hide: MaterialSymbolsVisibilityOffOutlineRounded,
   show: MaterialSymbolsVisibilityOutlineRounded,
   more: MaterialSymbolsMoreVert,
+  clear: MaterialSymbolsCloseRounded,
+  search: MaterialSymbolsSearchRounded,
 }
 
 export type Icon = keyof typeof iconMap
--- src/components/map/MapSearchInput.vue
+++ src/components/map/MapSearchInput.vue
@@ -1,29 +1,71 @@
 <template>
-  <div class="flex items-start gap-2">
-    <FormKit v-model="query" type="search" autocomplete="off" autofocus @keydown.enter="search" />
-    <BaseButton @click="search">Go</BaseButton>
+  <div class="flex items-start">
+    <FormKit
+      id="map-search-input"
+      v-model="query"
+      type="text"
+      :label="t('project.location')"
+      label-class="sr-only"
+      :placeholder="t('project.location')"
+      inner-class="rounded-r-none"
+      role="searchbox"
+      autocomplete="off"
+      auto-focus
+      :disabled="loading"
+      @keydown.enter="search"
+    >
+      <template #suffixIcon>
+        <button v-if="query" class="cursor-pointer hover:text-primary" @click="query = ''">
+          <IconRenderer icon="clear" />
+        </button>
+      </template>
+    </FormKit>
+    <BaseButton
+      classes="text-lg rounded-l-none"
+      :disabled="loading || query.trim() === ''"
+      @click="search"
+    >
+      <IconRenderer icon="search" />
+    </BaseButton>
   </div>
 </template>
 
 <script setup lang="ts">
 import BaseButton from '@/components/base/BaseButton.vue'
+import IconRenderer from '@/components/icon/IconRenderer.vue'
 import { useGeoService } from '@/composables/geo'
 import { useLogger } from '@/composables/log'
+import { useNotification } from '@/composables/notification'
 import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
 
+const { t } = useI18n()
 const { getGeoCode } = useGeoService()
+const { errorToast } = useNotification()
 const logger = useLogger('map')
 
 const query = ref('')
+const loading = ref(false)
 
 async function search() {
-  const data = await getGeoCode(query.value)
+  if (loading.value) return
+  if (!query.value.trim()) return
 
-  if (data && data.length > 0) {
-    const { lat, lon } = data[0]
-    logger.log(`Latitude: ${lat}, Longitude: ${lon}`)
-  } else {
-    logger.error('No results found for the query:', query.value)
+  loading.value = true
+  try {
+    const data = await getGeoCode(query.value)
+
+    if (data && data.length > 0) {
+      const { lat, lon } = data[0]
+      logger.log(`Latitude: ${lat}, Longitude: ${lon}`)
+    } else {
+      throw Error(`No results found for the query: ${query.value}`)
+    }
+  } catch (error) {
+    console.error(error)
+    errorToast(t('common.errorMessage'))
+  } finally {
+    loading.value = false
   }
 }
 </script>
