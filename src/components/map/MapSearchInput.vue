33dfe517c91484b976c1302eb8fb29fb06153a39
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-05-26 08:02:53 +0200

    error and loading state in composable

--- src/components/map/MapSearchInput.vue
+++ src/components/map/MapSearchInput.vue
@@ -40,32 +40,23 @@ import { ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
-const { getGeoCode } = useGeoService()
+const { loading, geocode, error, getGeoCode } = useGeoService()
 const { errorToast } = useNotification()
 const logger = useLogger('map')
 
 const query = ref('')
-const loading = ref(false)
 
 async function search() {
   if (loading.value) return
   if (!query.value.trim()) return
 
-  loading.value = true
-  try {
-    const data = await getGeoCode(query.value)
+  await getGeoCode(query.value)
 
-    if (data && data.length > 0) {
-      const { lat, lon } = data[0]
-      logger.log(`Latitude: ${lat}, Longitude: ${lon}`)
-    } else {
-      throw Error(`No results found for the query: ${query.value}`)
-    }
-  } catch (error) {
-    console.error(error)
-    errorToast(t('common.errorMessage'))
-  } finally {
-    loading.value = false
+  if (error.value) errorToast(t('common.errorMessage'))
+
+  if (geocode.value && geocode.value.length > 0) {
+    const { lat, lon } = geocode.value[0]
+    logger.log(`Latitude: ${lat}, Longitude: ${lon}`)
   }
 }
 </script>
--- src/composables/geo/index.ts
+++ src/composables/geo/index.ts
@@ -1,38 +1,52 @@
 import axios from 'axios'
+import { ref } from 'vue'
 
 export function useGeoService() {
-  return {
-    getGeoCode,
+  // TODO: Add response type or use nominatim library
+  // https://www.npmjs.com/package/nominatim-client
+  const geocode = ref()
+  const loading = ref<boolean>(false)
+  const error = ref<Error | null>(null)
+
+  /**
+   * Get geocoding information from OpenStreetMap's Nominatim service.
+   *
+   * @param search Search query for geocoding
+   * @returns An array of geocoding results.
+   */
+  async function getGeoCode(search: string) {
+    // Remove extra whitespace and replace spaces with plus signs
+    const query = search.trim().replace(/ /g, '+')
+
+    try {
+      // Reset state before making a new request
+      loading.value = true
+      geocode.value = null
+      error.value = null
+
+      // Fetch geocoding data from Nominatim
+      const response = await axios.get('https://nominatim.openstreetmap.org/search', {
+        params: {
+          q: query,
+          format: 'json',
+          addressdetails: 1,
+        },
+      })
+
+      if (response.status !== 200) throw new Error(response.statusText)
+
+      geocode.value = response.data
+    } catch (err) {
+      error.value = err instanceof Error ? err : new Error('An unknown error occurred')
+    } finally {
+      loading.value = false
+    }
   }
-}
 
-/**
- * Get geocoding information from OpenStreetMap's Nominatim service.
- *
- * TODO: Add response type or use nominatim library
- * https://www.npmjs.com/package/nominatim-client
- *
- * @param search Search query for geocoding
- * @returns An array of geocoding results.
- */
-async function getGeoCode(search: string) {
-  // Remove extra whitespace and replace spaces with plus signs
-  const query = search.trim().replace(/ /g, '+')
-
-  try {
-    // Fetch geocoding data from Nominatim
-    const response = await axios.get('https://nominatim.openstreetmap.org/search', {
-      params: {
-        q: query,
-        format: 'json',
-        addressdetails: 1,
-      },
-    })
-
-    if (response.status !== 200) throw new Error(response.statusText)
-
-    return response.data
-  } catch (error) {
-    throw error
+  return {
+    geocode,
+    loading,
+    error,
+    getGeoCode,
   }
 }
