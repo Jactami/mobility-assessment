563af5f9616c185cfd0b4421d52b3669b28780f4
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-06-07 14:58:49 +0200

    overpass query based on domain definition

--- src/composables/poi/overpass/OverpassQueryFactory.ts
+++ src/composables/poi/overpass/OverpassQueryFactory.ts
@@ -1,3 +1,5 @@
+import { DOMAINS } from '@/constants'
+import type { AreaCategory, AreaDomain } from '@/types'
 import { OverpassQueryBuilder } from './OverpassQueryBuilder'
 
 /**
@@ -6,14 +8,45 @@ import { OverpassQueryBuilder } from './OverpassQueryBuilder'
 export class OverpassQueryFactory {
   private static readonly builder: OverpassQueryBuilder = new OverpassQueryBuilder()
 
+  /**
+   * Creates an Overpass API query for the specified location and radius.
+   *
+   * @param lat The latitude of the center point.
+   * @param lon The longitude of the center point.
+   * @param radius The radius around the center point (in meters).
+   * @returns The Overpass API query string.
+   */
   static createQuery(lat: number, lon: number, radius: number) {
-    this.querySupermarkets(lat, lon, radius)
+    DOMAINS.forEach((domain) => {
+      this.queryDomain(domain, lat, lon, radius)
+    })
+
     return this.builder.build()
   }
 
-  private static querySupermarkets(lat: number, lon: number, radius: number) {
-    this.builder.add('shop=supermarket', lat, lon, radius)
+  /**
+   * Appends a specific domain of categories to the Overpass query.
+   *
+   * @param domain The domain to query.
+   * @param lat The latitude of the center point.
+   * @param lon The longitude of the center point.
+   * @param radius The radius around the center point (in meters).
+   */
+  private static queryDomain(domain: AreaDomain, lat: number, lon: number, radius: number) {
+    for (const category of domain.categories) {
+      this.queryCategory(category, lat, lon, radius)
+    }
   }
 
-  // More query methods will be added here...
+  /**
+   * Appends a specific category to the Overpass query.
+   *
+   * @param category The category to query.
+   * @param lat The latitude of the center point.
+   * @param lon The longitude of the center point.
+   * @param radius The radius around the center point (in meters).
+   */
+  private static queryCategory(category: AreaCategory, lat: number, lon: number, radius: number) {
+    this.builder.add(category.tag, lat, lon, radius)
+  }
 }
