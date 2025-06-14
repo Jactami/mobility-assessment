5d2c7958a08b4e706a94ce6e9ad375a1534d1c2e
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-06-14 15:13:07 +0200

    cleanup admin view

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,27 +1,3 @@
 <template>
   <div>TODO</div>
-  <pre>{{ profiles }}</pre>
-  <pre>{{ authStore.role }}</pre>
 </template>
-
-<script setup lang="ts">
-import { supabase } from '@/db'
-import { useAuthStore } from '@/stores/Auth'
-import { onMounted, ref } from 'vue'
-
-const authStore = useAuthStore()
-
-const profiles = ref()
-
-onMounted(async () => {
-  const { data, error } = await supabase.from('profiles').select()
-
-  if (error) {
-    console.error('Error fetching profiles:', error)
-    return
-  }
-
-  console.log(data)
-  profiles.value = data
-})
-</script>
