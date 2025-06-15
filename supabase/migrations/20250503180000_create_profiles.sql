adc511843309b8f3be88585857448ae487c32d12
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-06-15 13:27:56 +0200

    remove trigger to create profiles

--- supabase/migrations/20250503180000_create_profiles.sql
+++ supabase/migrations/20250503180000_create_profiles.sql
@@ -27,28 +27,6 @@ CREATE TRIGGER handle_updated_at_profiles
     FOR EACH ROW
     EXECUTE FUNCTION moddatetime('updated_at');
 
--- Inserts a row into public.profiles
-CREATE OR REPLACE FUNCTION public.handle_new_user()
-    RETURNS trigger
-    LANGUAGE plpgsql
-    SECURITY definer SET search_path = ''
-    AS $$
-BEGIN
-    INSERT INTO public.profiles (id, first_name, last_name, user_role)
-    VALUES (
-        new.id, new.raw_user_meta_data ->> 'first_name', 
-        new.raw_user_meta_data ->> 'last_name',
-        (new.raw_user_meta_data ->> 'user_role')::public.user_role
-    );
-    RETURN new;
-END;
-$$;
-
--- Trigger the function every time a user is created
-CREATE OR REPLACE TRIGGER on_auth_user_created
-    AFTER INSERT ON auth.users
-    FOR EACH ROW EXECUTE PROCEDURE public.handle_new_user();
-
 -- Ensure that only one user can have the role 'admin'
 CREATE UNIQUE INDEX only_one_admin
 ON public.profiles ((user_role))
---
+++ supabase/migrations/20250513110000_create_users.sql
@@ -0,0 +1,68 @@
+--
+-- Function create_user
+--
+CREATE OR REPLACE FUNCTION public.create_user(
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(100),
+    PASSWORD VARCHAR(100),
+    user_role public.user_role DEFAULT 'user',
+    user_id UUID DEFAULT NULL
+)
+RETURNS uuid
+AS $$
+DECLARE
+    encrypted_pw VARCHAR(255);
+    confirmation timestamp;
+BEGIN
+    IF user_id IS NULL THEN
+        user_id := gen_random_uuid();
+    END IF;
+
+    encrypted_pw := crypt(PASSWORD, gen_salt('bf'));
+    confirmation := now();
+
+    -- Insert into auth.users
+    INSERT INTO auth.users(
+        instance_id, id, aud, ROLE, email, encrypted_password, 
+        email_confirmed_at, recovery_sent_at, last_sign_in_at, 
+        confirmation_sent_at, raw_app_meta_data, raw_user_meta_data, 
+        created_at, updated_at, confirmation_token, email_change, 
+        email_change_token_new, recovery_token
+    )
+    VALUES (
+        '00000000-0000-0000-0000-000000000000', user_id, 'authenticated', 'authenticated', 
+        email, encrypted_pw, confirmation, confirmation, confirmation, confirmation,
+        '{}', jsonb_build_object(
+            'first_name', first_name,
+            'last_name', last_name,
+            'user_role', user_role
+        ),
+        now(), now(), '', '', '', ''
+    );
+
+    -- Insert into auth.identities
+    INSERT INTO auth.identities(
+        provider_id, user_id, identity_data, provider, 
+        last_sign_in_at, created_at, updated_at
+    )
+    VALUES (
+        gen_random_uuid(), user_id, 
+        format('{"sub":"%s","email":"%s"}', user_id::text, email)::jsonb, 
+        'email', now(), now(), now()
+    );
+
+    -- Insert directly into public.profiles
+    INSERT INTO public.profiles(
+        id, first_name, last_name, user_role
+    )
+    VALUES (
+        user_id, first_name, last_name, user_role
+    );
+
+    RETURN user_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+COMMENT ON FUNCTION public.create_user IS 'Helper function to add users during development.';
