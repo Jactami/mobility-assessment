587ebcad2baed83925ee72bd7e8f5fdb224b01ef
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-06-16 16:43:50 +0200

    simple map overlay

--- src/components/map/layers/MapLayerPois.vue
+++ src/components/map/layers/MapLayerPois.vue
@@ -3,7 +3,7 @@
     <Sources.OlSourceVector>
       <template v-for="poi in projectStore.pois" :key="poi.id">
         <!-- Marker -->
-        <Map.OlFeature>
+        <Map.OlFeature :properties="{ poi }">
           <Geometries.OlGeomPoint :coordinates="fromLonLat([poi.longitude, poi.latitude])" />
           <Styles.OlStyle>
             <Styles.OlStyleIcon
@@ -29,16 +29,57 @@
       </template>
     </Sources.OlSourceVector>
   </Layers.OlVectorLayer>
+
+  <Layers.OlVectorLayer>
+    <Sources.OlSourceVector>
+      <Interactions.OlInteractionSelect :filter="selectInteractionFilter" @select="handleSelect">
+        <Styles.OlStyle>
+          <!-- Leave empty to prevent default selection style -->
+        </Styles.OlStyle>
+      </Interactions.OlInteractionSelect>
+    </Sources.OlSourceVector>
+  </Layers.OlVectorLayer>
+  <Map.OlOverlay
+    v-if="selectedPoi"
+    :position="fromLonLat([selectedPoi.longitude, selectedPoi.latitude])"
+    :auto-pan="{
+      animation: {
+        duration: 200,
+      },
+    }"
+    positioning="bottom-center"
+    :offset="[0, -30]"
+  >
+    <div class="bg-white p-1">
+      <div>
+        {{ selectedPoi.label }}
+      </div>
+      <button @click="selectedPoi = null">x</button>
+    </div>
+  </Map.OlOverlay>
 </template>
 
 <script setup lang="ts">
 import { DOMAINS } from '@/constants'
 import type { Poi } from '@/db/types'
 import { useProjectStore } from '@/stores/Project'
+import type { Feature } from 'ol'
+import type { SelectEvent } from 'ol/interaction/Select'
 import { fromLonLat } from 'ol/proj'
-import { Geometries, Layers, Map, Sources, Styles } from 'vue3-openlayers'
+import { ref } from 'vue'
+import { Geometries, Interactions, Layers, Map, Sources, Styles } from 'vue3-openlayers'
 
 const projectStore = useProjectStore()
+
+const selectedPoi = ref<Poi | null>(null)
+
+function handleSelect(event: SelectEvent) {
+  selectedPoi.value = event.selected[0].getProperties().poi
+}
+
+function selectInteractionFilter(feature: Feature) {
+  return feature.get('poi') !== undefined
+}
 
 function getColorByDomain(poi: Poi) {
   return DOMAINS.find((domain) =>
