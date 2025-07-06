6795b0ab2119c4af6740d3812902334f5fd25acf
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-07-06 17:38:11 +0200

    score in project card

--- src/components/project/ProjectCard.vue
+++ src/components/project/ProjectCard.vue
@@ -3,14 +3,16 @@
     <BaseCard :animation="true" class="group flex flex-col">
       <!-- Card Header -->
       <div class="relative overflow-hidden">
-        <img
-          src="/img/placeholder.webp"
-          :alt="project.title"
-          loading="lazy"
-          class="aspect-video object-cover object-center transition-transform duration-300 group-hover:scale-110"
-        />
+        <div
+          class="flex aspect-[3/1] items-center justify-center bg-surface-container-high p-4 text-on-surface-inverse transition-transform duration-300 group-hover:scale-110"
+          :style="{ backgroundColor: bgColor }"
+        >
+          <div class="text-5xl font-semibold">
+            {{ project.score ? n(project.score * 100, 'rounded') : t('common.notAvailable') }}
+          </div>
+        </div>
         <span
-          class="absolute top-2 right-2 text-2xl text-primary transition-transform hover:scale-125"
+          class="absolute top-2 right-2 text-2xl text-secondary transition-transform hover:scale-125"
           :title="project.favorite ? t('project.removeFavorite') : t('project.addFavorite')"
           @click.prevent="emit('favorite')"
         >
@@ -21,7 +23,7 @@
 
       <!-- Card Body -->
       <div class="grow p-4">
-        <h2 class="line-clamp-2 h-[3rem] text-lg leading-tight font-medium">{{ project.title }}</h2>
+        <h2 class="line-clamp-2 h-[3rem] text-xl leading-tight font-medium">{{ project.title }}</h2>
         <div class="mt-4 text-on-surface-variant">
           <div class="truncate">{{ project.street }} {{ project.housenumber }}</div>
           <div class="truncate">{{ project.postcode }} {{ project.city }}</div>
@@ -43,6 +45,7 @@
 import BaseCard from '@/components/base/BaseCard.vue'
 import MenuPopup from '@/components/menu/MenuPopup.vue'
 import type { MenuActionItem } from '@/components/menu/types'
+import { useColorUtil } from '@/composables/util/color'
 import type { Project } from '@/db/types'
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
@@ -58,7 +61,10 @@ const emit = defineEmits<{
   (e: 'favorite'): void
 }>()
 
-const { d, t } = useI18n()
+const { d, n, t } = useI18n()
+const { scoreToColor } = useColorUtil()
+
+const bgColor = computed(() => (props.project.score ? scoreToColor(props.project.score) : ''))
 
 const menu = computed<MenuActionItem[]>(() => [
   {
