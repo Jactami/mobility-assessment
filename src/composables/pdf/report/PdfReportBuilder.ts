a4cd65ffd3f1f3bd4aa8dd9545183d4b28bfda4e
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-07-08 12:46:20 +0200

    clean up pdf builder

--- src/composables/pdf/report/PdfReportBuilder.ts
+++ src/composables/pdf/report/PdfReportBuilder.ts
@@ -137,29 +137,6 @@ export class PdfReportBuilder extends PdfBuilder {
   }
 
   /**
-   * Creates a header for the page, which includes a main header and an optional subheader.
-   * @param header - The main header for the page.
-   * @param subheader - The subheader for the page (optional).
-   * @returns The current instance of PdfReportBuilder for method chaining.
-   */
-  createPageHeader(header: string, subheader?: string): this {
-    if (subheader) {
-      this.createText(subheader, {
-        alignment: 'center',
-        font: 'bold',
-        y: this._config.padding.top + 10,
-      })
-    }
-
-    return this.createText(header, {
-      fontSize: 'lg',
-      color: 'primary',
-      alignment: 'center',
-      font: 'bold',
-    })
-  }
-
-  /**
    * Creates a section header for the PDF document, which includes a main header and an optional subheader.
    * @param header - The main header for the section.
    * @param options - Optional layout and styling options for the section header.
