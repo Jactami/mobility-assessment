c812b2bfa0805989614b1283a4581c79104f7a59
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-07-15 21:27:35 +0200

    update company data

--- src/composables/legal/index.ts
+++ src/composables/legal/index.ts
@@ -7,7 +7,7 @@ function getCompanyData() {
     name: 'Bayerische Gesellschaft für Wohneigentum – Digital mbH & Co. KG',
     address: 'Luitpoldstraße 53, 96052 Bamberg',
     email: 'info@bgw-digital.de',
-    phone: '+4995151954-0',
+    phone: '+49 951 51954-0',
     web: 'www.bgw-digital.de',
   }
 }
