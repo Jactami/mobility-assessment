79027aa90f0d0043ddb491932a3464017f9c0e42
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-09-22 20:34:57 +0200

    app logo

---
+++ src/components/app/AppBrand.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex items-center gap-x-0.5">
+    <img src="/logo.svg" :alt="t('app.title')" class="h-10 w-auto" aria-hidden="true" />
+    <div class="font-logo mt-1 text-2xl font-medium sm:text-3xl">
+      <span class="text-primary">MOBI</span>
+      <span class="text-secondary">METER</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
--- src/components/app/AppHeader.vue
+++ src/components/app/AppHeader.vue
@@ -1,11 +1,11 @@
 <template>
   <header
-    class="sticky top-0 z-10 border-b border-outline-variant bg-surface text-on-surface shadow-sm"
+    class="border-outline-variant bg-surface text-on-surface sticky top-0 z-10 border-b shadow-sm"
   >
-    <nav class="mx-auto flex h-16 items-center justify-between px-4 lg:px-10">
+    <nav class="mx-auto flex h-14 items-center justify-between gap-x-6 px-4 sm:h-16 lg:px-10">
       <div class="flex lg:flex-1">
         <RouterLink to="/">
-          <AppLogo />
+          <AppBrand />
         </RouterLink>
       </div>
 
@@ -24,8 +24,8 @@
 import AuthUserAvatar from '@/components/auth/AuthUserAvatar.vue'
 import { useAuthStore } from '@/stores/Auth'
 import { useI18n } from 'vue-i18n'
+import AppBrand from './AppBrand.vue'
 import AppDarkModeButton from './AppDarkModeButton.vue'
-import AppLogo from './AppLogo.vue'
 
 const authStore = useAuthStore()
 const { t } = useI18n()
