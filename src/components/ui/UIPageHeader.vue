f520295b97ad47a4b0aff4f137361dd5f8041133
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-10-01 09:20:41 +0200

    update header layout

--- src/components/ui/UIPageHeader.vue
+++ src/components/ui/UIPageHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="my-6">
-    <h1 class="text-3xl leading-tight font-bold text-pretty sm:text-4xl">{{ title }}</h1>
-    <div v-if="subtitle" class="mt-4 text-lg text-pretty text-on-surface-variant">
+    <h1 class="text-pretty text-3xl font-bold leading-tight sm:text-4xl">{{ title }}</h1>
+    <div v-if="subtitle" class="text-on-surface-muted mt-3 text-pretty text-lg font-medium">
       {{ subtitle }}
     </div>
   </header>
