44f0e867957b32424756b0d99297131a6b323b42
Christian Bartenstein <christian.bartenstein@googlemail.com>
2025-10-08 13:45:11 +0200

    fix poi type definition

--- src/db/types/index.ts
+++ src/db/types/index.ts
@@ -2,6 +2,11 @@ import type { Enums, Tables, TablesInsert } from './supabase'
 
 export type Profile = Tables<'profiles'>
 export type Project = Tables<'projects'>
-export type Poi = TablesInsert<'pois'>
+
+// manually overwrite footway type because supabase code gen can't handle nested arrays
+// TODO: not happy with this solution; check if there's a better way
+export type Poi = Omit<TablesInsert<'pois'>, 'footway'> & {
+  footway?: number[][]
+}
 
 export type UserRole = Enums<'user_role'>
