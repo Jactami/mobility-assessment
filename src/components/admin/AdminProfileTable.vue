1274edc79cf568375fe037de86b8a8112bb7a7ca
Christian Bartenstein <christian.bartenstein@googlemail.com>
2026-03-16 11:48:37 +0100

    show user state in profile table

--- src/components/admin/AdminProfileTable.vue
+++ src/components/admin/AdminProfileTable.vue
@@ -1,5 +1,16 @@
 <template>
-  <DataTable :data="profiles" :config="tableConfig" />
+  <DataTable :data="profiles" :config="tableConfig">
+    <template #column-is_disabled="{ formatted, value }">
+      <UIBadge v-if="value" severity="danger">
+        {{ formatted }}
+      </UIBadge>
+    </template>
+    <template #column-expires_at="{ formatted, value }">
+      <UIBadge v-if="value" :severity="new Date(String(value)) >= today ? 'warning' : 'danger'">
+        {{ formatted }}
+      </UIBadge>
+    </template>
+  </DataTable>
 
   <AdminProfileForm v-model:open="modalOpen" :profile="profileModel" @submit="handleSubmit" />
 </template>
@@ -7,8 +18,9 @@
 <script setup lang="ts">
 import DataTable from '@/components/table/DataTable.vue'
 import type TableConfig from '@/components/table/types'
+import UIBadge from '@/components/ui/UIBadge.vue'
 import type { Profile } from '@/db/types'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import AdminProfileForm from './AdminProfileForm.vue'
 import type { ProfileWithPassword } from './types'
@@ -23,10 +35,15 @@ const emit = defineEmits<{
   (e: 'delete', profile: Profile): void
 }>()
 
-const { t } = useI18n()
+const { d, t } = useI18n()
 
 const modalOpen = ref(false)
 const profileModel = ref<Profile>()
+
+const today = computed(() => {
+  const now = new Date()
+  return new Date(now.getFullYear(), now.getMonth(), now.getDate())
+})
 
 const tableConfig: TableConfig<Profile> = {
   columns: [
@@ -34,19 +51,33 @@ const tableConfig: TableConfig<Profile> = {
       key: 'last_name',
       label: t('user.lastName'),
       sort: 'raw',
-      width: 30,
+      width: 25,
     },
     {
       key: 'first_name',
       label: t('user.firstName'),
       sort: 'raw',
-      width: 30,
+      width: 25,
     },
     {
       key: 'email',
       label: t('user.email'),
       sort: 'raw',
-      width: 40,
+      width: 30,
+    },
+    {
+      key: 'is_disabled',
+      label: t('user.status'),
+      formatter: (disabled) => (disabled ? t('user.disabled') : ''),
+      sort: 'raw',
+      width: 10,
+    },
+    {
+      key: 'expires_at',
+      label: t('user.expiresAt'),
+      formatter: (date) => (date ? d(String(date), 'short') : ''),
+      sort: 'raw',
+      width: 10,
     },
   ],
   presort: { key: 'last_name', order: 'asc' },
