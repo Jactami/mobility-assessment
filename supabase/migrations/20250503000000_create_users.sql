ea613f9bfca2e7cad5878796510e9b2d7677a51a
Christian Bartenstein <christian.bartenstein@googlemail.com>
2026-09-10 18:05:33 +0200

    fix identity provider id

--- supabase/migrations/20250503000000_create_users.sql
+++ supabase/migrations/20250503000000_create_users.sql
@@ -21,12 +21,14 @@ BEGIN
         VALUES ('00000000-0000-0000-0000-000000000000', user_id, 'authenticated', 'authenticated', email, encrypted_pw, confirmation, confirmation, confirmation, confirmation, '{}', '{}', now(), now(), '', '', '', '');
     -- Insert into auth.identities
     INSERT INTO auth.identities(provider_id, user_id, identity_data, provider, last_sign_in_at, created_at, updated_at)
-        VALUES (gen_random_uuid(), user_id, format('{"sub":"%s","email":"%s"}', user_id::text, email)::jsonb, 'email', now(), now(), now());
+        VALUES (user_id, user_id, format('{"sub":"%s","email":"%s"}', user_id::text, email)::jsonb, 'email', now(), now(), now());
     -- Insert into public.profiles
     INSERT INTO public.profiles(id, first_name, last_name, email, user_role, is_disabled, expires_at)
         VALUES (user_id, first_name, last_name, email, user_role, is_disabled, expires_at)
     RETURNING
-        * INTO new_profile;
+        *
+    INTO
+        new_profile;
     -- Return the new profile
     RETURN new_profile;
 END;
